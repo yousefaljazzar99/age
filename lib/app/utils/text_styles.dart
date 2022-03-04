1f4f2c8e38d759b62d7572547c7ea8ec5bcd3dbc
yousefaljazzar99 <joseph.ana19221@gmail.com>
2022-03-04 11:26:23 +0200

    first

---
+++ lib/app/utils/text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TextStyleManager {
+  // ignore: constant_identifier_names
+  static const TextStyle LARGE_TEXT = TextStyle(
+    fontSize: 64,
+    color: Colors.white,
+  );
+
+  // ignore: constant_identifier_names
+  static const TextStyle MEDIUM_TEXT = TextStyle(
+    fontSize: 24,
+    color: Colors.white,
+  );
+
+  // ignore: constant_identifier_names
+  static const TextStyle SMALL_TEXT = TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+  );
+}
